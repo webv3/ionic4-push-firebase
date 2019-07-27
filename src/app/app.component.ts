7a352422d3036bd9e3ba24a4055ea1586088e912
webv3 <webv3@hotmail.com>
2019-07-27 08:15:10 -0300

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { FCM } from '@ionic-native/fcm/ngx';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: 'app.component.html'
+})
+export class AppComponent {
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar,
+        private fcm: FCM,
+        private router: Router
+    ) {
+        this.initializeApp();
+    }
+
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+
+            this.fcm.subscribeToTopic('people');
+
+            this.fcm.getToken().then(token => {
+                console.log(token);
+            });
+
+            this.fcm.onNotification().subscribe(data => {
+                console.log(data);
+                if (data.wasTapped) {
+                    console.log('Received in background');
+                    this.router.navigate([data.landing_page, data.price]);
+                } else {
+                    console.log('Received in foreground');
+                    this.router.navigate([data.landing_page, data.price]);
+                }
+            });
+
+            this.fcm.onTokenRefresh().subscribe(token => {
+                console.log(token);
+            });
+
+            // this.fcm.unsubscribeFromTopic('marketing');
+        });
+    }
+}
